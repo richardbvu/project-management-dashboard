33c04c7874b1812b7e3f9f7459b87f21804f5eb7
Richard <richardvu93@gmail.com>
2024-09-13 11:15:09 -0700

    navbar

---
+++ src/app/(components)/Sidebar/index.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { useState } from "react";
+
+const Sidebar = () => {
+  const [showProjects, setShowProjects] = useState(true);
+  return <div>Sidebar</div>;
+};
+export default Sidebar;
--- src/app/dashboardWrapper.tsx
+++ src/app/dashboardWrapper.tsx
@@ -1,8 +1,20 @@
 import React from "react";
+import Navbar from "@/app/(components)/Navbar";
+import Sidebar from "@/app/(components)/Sidebar";
 
 const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex min-h-screen w-full bg-gray-50">DashboardWrapper</div>
+    <div className="flex min-h-screen w-full bg-gray-50">
+      {/* SIDE BAR */}
+      <Sidebar />
+      {/* NAVBAR */}
+      <main
+        className={`dark:bg-dark-bg flex w-full flex-col bg-gray-50 md:pl-64`}
+      >
+        <Navbar />
+        {children}
+      </main>
+    </div>
   );
 };
 export default DashboardWrapper;
