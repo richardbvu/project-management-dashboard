142423ebd14f2ef4172d4fb717bf0cf159c396fc
Richard <richardvu93@gmail.com>
2024-09-22 09:15:33 -0700

    api calls in frontend

--- client/src/app/state/api.ts
+++ client/src/app/state/api.ts
@@ -1,10 +1,126 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+export interface Project {
+  id: number;
+  name: string;
+  description?: string;
+  startDate?: string;
+  endDate?: string;
+}
+
+export enum Priority {
+  Urgent = "Urgent",
+  High = "High",
+  Medium = "Medium",
+  Low = "Low",
+  Backlog = "Backlog",
+}
+
+export enum Status {
+  ToDo = "To Do",
+  WorkInProgress = "Work In Progress",
+  UnderReview = "Under Review",
+  Completed = "Completed",
+}
+
+export interface User {
+  userId: number;
+  username: string;
+  email: string;
+  profilePictureUrl?: string;
+  cognitoId?: string;
+  teamId: number;
+}
+
+export interface Attachment {
+  id: number;
+  fileURL: string;
+  fileName: string;
+  taskId: number;
+  uploadedById: number;
+}
+
+export interface Task {
+  id: number;
+  title: string;
+  description?: string;
+  status?: Status;
+  priority?: Priority;
+  tags?: string;
+  startDate?: string;
+  dueDate?: string;
+  points?: number;
+  projectId?: number;
+  authorUserId?: number;
+  assignedUserId?: number;
+
+  author?: User;
+  assignee?: User;
+  comments?: Comment[];
+  attachments?: Attachment[];
+}
+
 export const api = createApi({
+  // baseQuery: http://localhost:8000
   baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL }),
   reducerPath: "api",
-  tagTypes: [],
-  endpoints: (build) => ({}),
+  // same as providesTags:
+  tagTypes: ["Projects", "Tasks"],
+  // endpoints created in backend, to make call in frontend
+  endpoints: (build) => ({
+    // redux toolkit below, api request via build.query
+    // Get back Project[] typescript schema from backend
+    getProjects: build.query<Project[], void>({
+      // tags on http://localhost:8000/projects
+      query: () => "projects",
+      // when grab api request of "projects", we get data in frontend,
+      // then redux toolkit query will identify where data is stored in,
+      // in this example, its from providesTags: ["Projects"]
+      providesTags: ["Projects"],
+    }),
+    // mutation because its a POST request
+    createProject: build.mutation<Project, Partial<Project>>({
+      query: (project) => ({
+        url: "projects",
+        method: "POST",
+        body: project,
+      }),
+      invalidatesTags: ["Projects"],
+      // if create a project, redux toolkit will automatically get the new list of projects
+      // INCLUDING the new project created and RECALL getProjects again
+      // allowing to always get updated version of projects
+    }),
+    getTasks: build.query<Task[], { projectId: number }>({
+      query: ({ projectId }) => `tasks?projectId=${projectId}`,
+      providesTags: (result) =>
+        result
+          ? result.map(({ id }) => ({ type: "Tasks" as const, id }))
+          : [{ type: "Tasks" as const }],
+    }),
+    createTask: build.mutation<Task, Partial<Task>>({
+      query: (tasks) => ({
+        url: "tasks",
+        method: "POST",
+        body: tasks,
+      }),
+      invalidatesTags: ["Tasks"],
+    }),
+    updateTaskStatus: build.mutation<Task, { taskId: number; status: string }>({
+      query: ({ taskId, status }) => ({
+        url: `tasks/${taskId}/status`,
+        method: "PATCH",
+        body: { status },
+      }),
+      invalidatesTags: (result, error, { taskId }) => [
+        { type: "Tasks", id: taskId },
+      ],
+    }),
+  }),
 });
 
-export const {} = api;
+export const {
+  useGetProjectsQuery,
+  useCreateProjectMutation,
+  useGetTasksQuery,
+  useCreateTaskMutation,
+} = api;
