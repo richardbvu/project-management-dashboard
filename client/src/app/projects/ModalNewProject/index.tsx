c0ef9283dcb8faa90f93ebb880e4697c22ae4799
Richard <richardvu93@gmail.com>
2024-09-30 11:22:49 -0700

    modalnewtask

--- client/src/app/projects/ModalNewProject/index.tsx
+++ client/src/app/projects/ModalNewProject/index.tsx
@@ -61,19 +61,21 @@ const ModalNewProject = ({ isOpen, onClose }: Props) => {
           value={description}
           onChange={(e) => setDescription(e.target.value)}
         />
-        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 sm:gap-2"></div>
-        <input
-          type="date"
-          className={inputStyles}
-          value={startDate}
-          onChange={(e) => setStartDate(e.target.value)}
-        />
-        <input
-          type="date"
-          className={inputStyles}
-          value={endDate}
-          onChange={(e) => setEndDate(e.target.value)}
-        />
+        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 sm:gap-2">
+          <input
+            type="date"
+            className={inputStyles}
+            value={startDate}
+            onChange={(e) => setStartDate(e.target.value)}
+          />
+          <input
+            type="date"
+            className={inputStyles}
+            value={endDate}
+            onChange={(e) => setEndDate(e.target.value)}
+          />
+        </div>
+
         <button
           type="submit"
           className={`mt-4 flex w-full justify-center rounded-md border border-transparent bg-blue-primary px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 ${!isFormValid() || isLoading ? "cursor-not-allowed opacity-50" : ""}`}
